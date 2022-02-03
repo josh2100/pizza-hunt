22a3c93c6d06032d098ae5bacd3e663509f31564
joshgit <jschermann91@gmail.com>
2022-02-03 17:15:07 -0600

    offline accessibility

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -63,6 +63,8 @@ const handlePizzaSubmit = (event) => {
     })
     .catch((err) => {
       console.log(err);
+      // If internet loss, save with indexDB
+      saveRecord(formData);
     });
 };
 
